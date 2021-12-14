43f554786e571d089cf329e184619dfed5447d35
Pedro Chedid <pedro.chedid@a55.tech>
2021-12-13 22:25:37 -0300

    Feat: Adjust Prepare_salary class

--- bussines/sf_machiine.py/prepair_salary.py
+++ bussines/sf_machiine.py/prepair_salary.py
@@ -1,15 +1,22 @@
 from models.fixed_debits import FixedDebits
+from models.floated_debits import FloatedDebits
+from get_env import PO_INVESTMENT, PO_BIG_SAVE
 
 
 class Prepare_salary:
     
-    def savings(self):
-        porcent_save = self.salary * 0.05
+    def _calculate_percentage(self):
+        self.invest = self.salary * PO_INVESTMENT
+        self.net = self.gross - self._get_fixed_debits()- self._get_floated_debits()
+        self.porcent_big_save = self.net * PO_BIG_SAVE
     
-    def fixed_debits(self):
-        fd = FixedDebits()
+    def _get_fixed_debits(self):
+        fixed = FixedDebits()
     
+    def _get_floated_debits(self):
+        floated = FloatedDebits()
     
+    @staticmethod
     def run(self, salary):
-        self.salary = salary
-        self.savings()
+        self.gross = salary
+        self._calculate_percentage()
