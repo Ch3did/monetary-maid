7c06e28b7439755b2f67c0604b8239dcab2e6384
Pedro Chedid <pedro.chedid@a55.tech>
2021-12-13 21:52:51 -0300

    Feat: Update get_env to get taxes

--- get_env.py
+++ get_env.py
@@ -2,7 +2,11 @@ import os
 
 os.environ(".env")
 
+#Nubank Data 
 TAX_ID=os.environ.get('TAX_ID')
 PASSWORD=os.environ.get('PASSWORD')
 FOLDER_PATH=os.environ.get('FOLDER_PATH')
 
+# PLR
+PERCENTAGE_OF_INVESTMENT=os.environ.get('PERCENTAGE_OF_INVESTMENT')
+PERCENTAGE_OF_BIG_SAVE=os.environ.get('PERCENTAGE_OF_BIG_SAVE')
