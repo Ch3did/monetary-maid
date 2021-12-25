527d2fece5e5691437d474fb012021ad2b9cff82
Pedro Chedid <pedro.chedid@a55.tech>
2021-12-25 15:59:49 -0300

    ci: Added .env logic

--- get_env.py
+++ get_env.py
@@ -1,12 +1,16 @@
 import os
 
-os.environ(".env")
+from dotenv import load_dotenv
+import os
 
 #Nubank Data 
-TAX_ID=os.environ.get('TAX_ID')
-PASSWORD=os.environ.get('PASSWORD')
-FOLDER_PATH=os.environ.get('FOLDER_PATH')
+# TAX_ID=os.environ.get['TAX_ID']
+# PASSWORD=os.environ.get['PASSWORD']
+# FOLDER_PATH=os.environ.get['FOLDER_PATH']
+
+load_dotenv()
 
 # PLR
-PERCENTAGE_OF_INVESTMENT=os.environ.get('PERCENTAGE_OF_INVESTMENT')
-PERCENTAGE_OF_BIG_SAVE=os.environ.get('PERCENTAGE_OF_BIG_SAVE')
+PO_INVESTMENT=os.environ.get('PO_INVESTMENT')
+PO_BIG_SAVE=os.environ.get('PO_BIG_SAVE')
+PO_MONTH_EMERGENCY=os.environ.get('PO_MONTH_EMERGENCY')
