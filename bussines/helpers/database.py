f4ba43d6a7b39f940720702a68feae031395193c
Pedro Chedid <pedro.chedid@a55.tech>
2022-01-18 03:14:20 -0300

    feat: class to handle database conections

---
+++ bussines/helpers/database.py
@@ -0,0 +1,20 @@
+from os import stat
+
+from loguru import logger
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from get_env import DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER, DEBUG
+
+
+class Database:
+    def __init__(self):
+        self.engine = create_engine(self._make_endpoint(), echo=bool(DEBUG))
+        self.session = self.make_session()
+
+    def _make_endpoint(self):
+        return f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+    def make_session(self):
+        Session = sessionmaker(bind=self.engine)
+        return Session
