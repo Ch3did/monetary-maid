d376a6b4685ff427f21fed993d922bbcf6444721
Pedro Chedid <pedro.chedid@a55.tech>
2022-01-18 03:16:31 -0300

    feat: make basic test's

--- test/conftest.py
+++ test/conftest.py
@@ -1,12 +1,13 @@
+import os
+import sys
+
 import pytest
-import os 
+
 from bussines.machine.prepair_salary import PrepareSalary
 
-import sys
 sys.path.insert(0, os.path.abspath("code/pessoal/saved-future/"))
 
 
 @pytest.fixture
 def get_PrepareSalary():
     return PrepareSalary(3000)
-
