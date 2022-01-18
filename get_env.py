116f70ca147fb26d5fe83af5517129f9c86ecabf
Pedro Chedid <pedro.chedid@a55.tech>
2022-01-18 03:11:24 -0300

    feat: added new env variables

--- get_env.py
+++ get_env.py
@@ -1,16 +1,24 @@
 import os
 
 from dotenv import load_dotenv
-import os
 
-#Nubank Data 
+# Nubank Data
 # TAX_ID=os.environ.get['TAX_ID']
 # PASSWORD=os.environ.get['PASSWORD']
 # FOLDER_PATH=os.environ.get['FOLDER_PATH']
 
 load_dotenv()
 
+DEBUG = os.environ.get("DEBUG")
+
 # PLR
-PO_INVESTMENT=os.environ.get('PO_INVESTMENT')
-PO_BIG_SAVE=os.environ.get('PO_BIG_SAVE')
-PO_MONTH_EMERGENCY=os.environ.get('PO_MONTH_EMERGENCY')
+PO_INVESTMENT = os.environ.get("PO_INVESTMENT")
+PO_BIG_SAVE = os.environ.get("PO_BIG_SAVE")
+PO_MONTH_EMERGENCY = os.environ.get("PO_MONTH_EMERGENCY")
+
+# DB Conection
+DB_NAME = os.environ.get("DB_NAME")
+DB_USER = os.environ.get("DB_USER")
+DB_PASSWORD = os.environ.get("DB_PASSWORD")
+DB_HOST = os.environ.get("DB_HOST")
+DB_PORT = os.environ.get("DB_PORT")
