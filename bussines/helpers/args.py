90a6ce4686a1df1c264b69efff0251e278c87811
pedro chedid <phchedidk@gmail.com>
2022-02-17 01:12:41 -0300

    feat: Segragate arguments

--- bussines/helpers/args.py
+++ bussines/helpers/args.py
@@ -1,8 +1,39 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("amount", type=int, nargs="+", help="The amount of money you gain")
-
+parser.add_argument(
+    "-g",
+    "--get",
+    action="store_true",
+    required=False,
+    help="Get your last wallet",
+)
+parser.add_argument(
+    "-p",
+    "--period",
+    type=str,
+    nargs="+",
+    required=False,
+    help="Use for get especific register",
+)
+parser.add_argument(
+    "-r",
+    "--register",
+    type=int,
+    nargs="+",
+    required=False,
+    help="Register your wallet",
+)
+parser.add_argument(
+    "-d",
+    "--debits",
+    action="store_true",
+    required=False,
+    help="Get your debits",
+)
 
 def get_args():
-    return parser.parse_args()
+    return vars(parser.parse_args())
+
+
+# TODO: Parsear argumento para busca específica de carteira
