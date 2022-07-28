45eaef4a6b1dab021f01c8ff8b32a6c1158fc1b1
Ch3did <phchedidk@gmail.com>
2022-07-28 02:03:37 -0300

    wip: fixing click cli

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import click
 
 from monetary_maid.view import MonetaryView
 
-mv = MonetaryView()
+# TODO: Toda vez que descomento o import da classe da view o click começa a
+# apresentar comportamentos estranhos... Será que é pelo click ser escrito em funcional?
+# Validar isso.
+
 
 # Groups
 @click.group("get", help="Monetary Get: Get some info in the wallet")
@@ -18,12 +21,14 @@ def mput():
 
 @mget.command("debit", help="Get debits info")
 def get_debits():
+    # mv = MonetaryView()
     click.echo("debits")
 
 
 @mget.command("wallet", help="Get wallet info")
 @click.option("--period", "-p", help="Get info for a period", type=click.DateTime())
 def get_wallet(period):
-    click.echo(period.strftime("%m-%Y"))
+    # mv = MonetaryView()
     if not period:
-        period = arrow.now().format("MM-YYYY")
+        period = arrow.now()
+    click.echo(period)
