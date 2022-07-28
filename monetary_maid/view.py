547ff897b6386562ccd65ebb79611c7b5df31015
Ch3did <phchedidk@gmail.com>
2022-07-28 02:04:30 -0300

    wip: removed session as atribute | update  register_month_salary logic

--- monetary_maid/view.py
+++ monetary_maid/view.py
@@ -13,9 +13,6 @@ args = get_args()
 
 
 class MonetaryView:
-    def __init__(self):
-        self.conn = Database().session()
-
     def get_money_info(self, month=arrow.now().strftime("%Y-%m")):
         try:
             if wallet := self.conn.query(Wallet).filter(Wallet.date == month).first():
@@ -53,58 +50,20 @@ class MonetaryView:
                 return False
         return True
 
-    def register_month_salary(self):
+    def register_month_salary(self, salary):
         if not self._validate_date():
             return logger.error(
                 "You can't register a new month salary, because you already registered one."
             )
         try:
-            logger.info(f"Salary: {args.get('register')[0]}")
-
-            machine = WalletMachine(args["register"])
-
-            wallet = Wallet(
-                gross=machine.gross,
-                fixed_debits=machine.fixed_debits,
-                floated_debits=machine.floated_debits,
-                net=machine.net,
-                invest=machine.invest,
-                big_save=machine.big_save,
-                month_emergency=machine.month_emergency,
-                available=machine.available,
-                timestamp=machine.time_stamp,
-                date=machine.date,
-                debits_details=sum_dict(
-                    machine.fixed_debits_details, machine.floated_debits_details
-                ),
-            )
-            self.conn.add(wallet)
-            self.conn.commit()
+            wallet = WalletMachine().save_new_wallet(salary)
             logger.info(f"Your Wallet is saved!")
-            total = float(machine.floated_debits) + float(machine.fixed_debits)
+            total = float(wallet.floated_debits) + float(wallet.fixed_debits)
             logger.info(f"Bills: R${total:.2f}")
-            logger.info(f"Net: R${machine.net:.2f}")
-            logger.info(f"Invest: R${machine.invest:.2f}")
-            logger.info(f"Big Save: R${machine.big_save:.2f}")
-            logger.info(f"Month Emergency: R${machine.month_emergency:.2f}")
-            logger.info(f"Available: R${machine.available:.2f}")
+            logger.info(f"Net: R${wallet.net:.2f}")
+            logger.info(f"Invest: R${wallet.invest:.2f}")
+            logger.info(f"Big Save: R${wallet.big_save:.2f}")
+            logger.info(f"Month Emergency: R${wallet.month_emergency:.2f}")
+            logger.info(f"Available: R${wallet.available:.2f}")
         except Exception as e:
             logger.error(f"{e}")
-
-
-if __name__ == "__main__":
-    c = CLI()
-    if args["get"]:
-        if not args["period"]:
-            c.get_money_info()
-        else:
-            c.get_money_info(args["period"][0])
-
-    elif args["register"]:
-        c.register_month_salary()
-    elif args["debits"]:
-        c.get_debits_info()
-    else:
-        logger.error("You need to specify a option")
-
-    c.conn.close()
