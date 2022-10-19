5155d87a266d3d0ceeeff35c98fb861220070402
Ch3did <phchedidk@gmail.com>
2022-10-18 21:21:42 -0300

    feat: update relations | changed pynubank method

--- src/bussines/nubank.py
+++ src/bussines/nubank.py
@@ -2,6 +2,7 @@ import arrow
 from loguru import logger
 from pynubank import Nubank
 
+
 from src.bussines.atm import Statment_ATM
 from src.get_env import FOLDER_PATH, PASSWORD, TAX_ID
 from src.helpers.database import Database
@@ -17,10 +18,23 @@ class Nubank_API(Statment_ATM):
         self.conn = Database().session()
 
     def _get_value(self):
-        return self.nu.get_account_balance()
+        return self.nu.get_account_statements()
 
     def _get_transactions(self):
-        return self.nu.get_account_feed()
+        return self.nu.get_account_statements()
+
+    def _update_relations(self, type, detail):
+        id_establishment = 1
+        id_type = self.add_type(type)
+        if detail.find("R$") > 0:
+            position = detail.find("R$")
+            id_establishment = self.add_establishment(
+                (detail[:position]).lstrip(" ").rstrip(".- ").capitalize()
+            )
+
+        # TODO: Setar valor default para establishment == None
+
+        return {"type_id": id_type, "establishment_id": id_establishment}
 
     def _get_amount(self, item):
         position = item["detail"].find("$")
@@ -32,6 +46,7 @@ class Nubank_API(Statment_ATM):
         try:
             transactions = self._get_transactions()
             for item in transactions:
+                # Formata dados recebidos para análise
                 transaction = {
                     "amount": float(item.get("amount", self._get_amount(item))),
                     "checknum": item["id"],
@@ -40,12 +55,14 @@ class Nubank_API(Statment_ATM):
                     "date": item["postDate"],
                     "typename": item["__typename"],
                 }
+                relation_values = self._update_relations(
+                    transaction["title"], transaction["detail"]
+                )
+                transaction["type_id"] = relation_values["type_id"]
+                transaction["establishment_id"] = relation_values["establishment_id"]
 
                 self.add_statment(transaction)
                 # Valida estabelecimento de compra pra alimentar Establishments
-                if transaction["title"] == "Compra no débito":
-                    self.add_establishment(
-                        transaction["detail"][: transaction["detail"].find("-") - 1]
-                    )
+
         except Exception as error:
             logger.error(error)
