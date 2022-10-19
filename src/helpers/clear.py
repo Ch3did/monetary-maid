c9f332936ba99340f4d13810089aa3e425f702bb
Ch3did <phchedidk@gmail.com>
2022-10-18 21:22:14 -0300

    feat: pre-commit

--- src/helpers/clear.py
+++ src/helpers/clear.py
@@ -1,10 +1,13 @@
 import os
 
+
 def clean_output(func):
     os.system("clear")
+
     def decorator():
         func()
         input("Press Enter to continue...")
         os.system("clear")
         return
+
     return decorator
