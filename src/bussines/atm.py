8fbb976d86193e2ec2ac9c130d63209aec4ae961
Ch3did <phchedidk@gmail.com>
2022-10-19 02:38:58 -0300

    feat: added get_stablishment logic

--- src/bussines/atm.py
+++ src/bussines/atm.py
@@ -4,8 +4,6 @@ from src.helpers.database import Database
 from src.helpers.validators import ATMValidatorException
 from src.models.atm import Establishments, Statment, StatmentTypes
 
-# TODO: criar methodo para atualizar os dados de estabelecimento
-
 
 # TODO: Criar lógica para pegar geolocalização dos estabelecimentos
 
@@ -15,7 +13,8 @@ class Statment_ATM:
 
         self.conn = Database().session()
 
-    def update_establishment(self, establishment, details, geolocation):
+    # Put
+    def update_establishment_datails(self, establishment, details, geolocation):
         if id := (
             self.conn.query(Establishments.id)
             .filter(Establishments.name == establishment)
@@ -42,13 +41,13 @@ class Statment_ATM:
     def add_establishment(self, establishment):
         if values := (
             self.conn.query(Establishments.id)
-            .filter(Establishments.name == establishment)
+            .filter(Establishments.name == establishment.lower())
             .all()
         ):
             return int(values[0][0])
 
-        self.conn.add(Establishments(name=establishment))
-        logger.info(f"Finded new establishment: {establishment}")
+        self.conn.add(Establishments(name=establishment.lower()))
+        logger.info(f"Finded new establishment: {establishment.title()}")
         self.conn.commit()
         self.conn.close()
         return self.add_establishment(establishment)
@@ -82,17 +81,22 @@ class Statment_ATM:
             for negative_sentence in negative
             if negative_sentence in type.lower()
         ]:
-            title_type.is_negative = True
-            title_type.is_math_use = True
+            is_negative = True
+            is_math_use = True
         elif [
             positive_sentence
             for positive_sentence in positive
             if positive_sentence in type.lower()
         ]:
-            title_type.is_negative = False
-            title_type.is_math_use = True
+            is_negative = False
+            is_math_use = True
         else:
-            title_type.is_math_use = False
+            is_negative = None
+            is_math_use = False
+
+        title_type = StatmentTypes(
+            name=type, is_math_use=is_math_use, is_negative=is_negative
+        )
 
         self.conn.add(title_type)
         logger.info(f"Novo tipo {type} inserido no banco")
@@ -126,3 +130,44 @@ class Statment_ATM:
             logger.info(f"inserido no banco: {item['title']}")
             self.conn.commit()
             self.conn.close()
+
+    # Get
+    def get_establishment_info(self, establishment_name):
+        if data := (
+            self.conn.query(Establishments)
+            .filter(Establishments.name == establishment_name.lower())
+            .first()
+        ):
+            return {
+                "name": data.name,
+                "created_at": data.created_at,
+                "details": data.detail,
+                "address": data.address,
+                "is_pf": data.is_pf,
+            }
+
+        return None
+
+    def get_establishment_statment(self, establishment_name, period):
+        value = 0
+        if id := (
+            self.conn.query(Establishments.id)
+            .filter(Establishments.name == establishment_name.lower())
+            .first()
+        ):
+            if data := (
+                self.conn.query(Statment.amount, Statment.statment_type_id)
+                .filter(Statment.establishment_id == id[0])
+                .filter(Statment.date >= period)
+                .all()
+            ):
+
+                for item in data:
+                    transaction_type_id = item[1]
+                    if (self.conn.query(StatmentTypes.is_negative).filter(StatmentTypes.id == transaction_type_id).first())[0]:
+                        value -= float(item[0])
+                    else:
+                        value += float(item[0])
+
+                return value
+        return None
