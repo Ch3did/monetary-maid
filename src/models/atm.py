540bac6dec40ff4130b8e0cb08f56ff93b0daf56
Ch3did <phchedidk@gmail.com>
2022-10-18 21:19:34 -0300

    feat: update statment model | add statment_type

--- src/models/atm.py
+++ src/models/atm.py
@@ -31,13 +31,12 @@ class Statment(Base):
     __tablename__ = "statments"
     id = Column(Integer, primary_key=True)
     checknum = Column(String(100), nullable=False)
-    title = Column(String(60), nullable=False)
     detail = Column(Text, nullable=False)
     date = Column(DateTime, nullable=False)
     typename = Column(String(100), nullable=False)
     amount = Column(Numeric, nullable=False)
-    is_negative = Column(Boolean)
     # ForeignKey
+    statment_type_id = Column(Integer, ForeignKey("statment_types.id"))
     categories_id = Column(Integer, ForeignKey("categories.id"))
     establishment_id = Column(Integer, ForeignKey("establishments.id"))
 
@@ -55,6 +54,21 @@ class Establishments(Base):
     )  # Data que foi alterado
     detail = Column(Text, nullable=True)
     address = Column(String(60), nullable=True)
+    statment = relationship("Statment")
+    is_visible = Column(Boolean, default=True)
+    is_pf = Column(Boolean, default=False)
+
+    # TODO: added Typo de estabelecimento
+
+
+class StatmentTypes(Base):
+    __tablename__ = "statment_types"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(60), nullable=False)
+    description = Column(Text, nullable=True)
+    is_math_use = Column(Boolean, default=False)
+    is_negative = Column(Boolean)
     statment = relationship("Statment")
 
 
