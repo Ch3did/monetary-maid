5813a625beaa18e9ab1efca0ad7856d09ba3419b
Ch3did <phchedidk@gmail.com>
2022-10-21 01:56:22 -0300

    feat: added TODO

--- src/helpers/clear.py
+++ src/helpers/clear.py
@@ -1,6 +1,8 @@
 import os
 
 
+# TODO: Pegar nome da função e tratar pra printar antes de cada execussão do clean output (vide create_category_view)
+# TODO: ALTERAR O CLEANOUTPUT PRA LIMPAR E SANATIZAR OS DADOS A SEREM USADOS DENTRO DAS VIEWS
 def clean_output(func):
     os.system("clear")
 
