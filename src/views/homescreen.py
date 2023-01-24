9f0a8f927c6499d824b6ccafe7fede959d34f2fe
Ch3did <phchedidk@gmail.com>
2023-01-23 23:23:33 -0300

    feat: update home view

--- src/views/homescreen.py
+++ src/views/homescreen.py
@@ -30,15 +30,14 @@ def make_homescreen(search_type):
                 total = abs(item.expected)
                 title = item.name.title()
                 if total > count:
-
                     filled_len = int(round(bar_len * float(count) / float(total)))
-
                     bar = "#" * filled_len + "-" * (bar_len - filled_len)
-
-                    text_bar = f"{title.ljust(15)}:  [{bar}] | Used: R${total-count:,.2f} | Remain: {(total-count):,.2f}\n"
                 else:
                     bar = "#".ljust(bar_len, "#")
-                    text_bar = f"{title.ljust(15)}:  [{bar}] | Used: R${total:,.2f} | Remain: {0:,.2f}\n"
+
+                text_bar = (
+                    f"{title.ljust(15)}:  [{bar}] | partials: {total-count:,.2f}\n"
+                )
                 sys.stdout.write(text_bar)
 
     except Exception as error:
